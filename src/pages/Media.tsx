b2d5ca002974eeed42662de634eb9d0b270d056b
baltadar <josephrichards293@gmail.com>
2026-04-28 13:48:09 +0300

    Update Media.tsx
    small

--- src/pages/Media.tsx
+++ src/pages/Media.tsx
@@ -12,6 +12,7 @@ const videos = [
 
 const photos = [
   { src: "https://res.cloudinary.com/dbgcwdes6/image/upload/v1777268144/Screenshot_2026-04-27_at_08.35.22_cww6a4.png", alt: "Hon. Cecil Miller on the campaign trail" },
+  { src: "https://res.cloudinary.com/dbgcwdes6/image/upload/v1777373230/WhatsApp_Image_2026-04-27_at_23.30.03_i7lrcz.jpg", alt: "Hon. Cecil Miller on the campaign trail" },
   { src: "https://res.cloudinary.com/dbgcwdes6/image/upload/v1777268161/WhatsApp_Image_2026-04-25_at_12.37.04_lhpw1t.jpg", alt: "Hon. Cecil Miller engaging with Nairobi residents" },
   { src: "https://res.cloudinary.com/dbgcwdes6/image/upload/v1777271182/Screenshot_2026-04-27_at_09.19.09_jdaf2k.png", alt: "Hon. Cecil Miller on the campaign trail" },
   { src: "https://res.cloudinary.com/dbgcwdes6/image/upload/v1777271182/Screenshot_2026-04-27_at_09.21.08_zllvwg.png", alt: "Hon. Cecil Miller on the campaign trail" },
